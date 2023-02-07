d0f576660651c49a66009ec1b96f5cf5cd4c2782
Jonas <vergessxner@gmail.com>
2023-02-07 12:36:32 +0000

    feat: Added Interacte

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,28 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import { useState } from 'react'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
-  var console = () => {
 
+  var [activBrand, setActivBrand] = useState('Audi');
+
+  var herstellerArray = ['Audi', 'BMW', 'Mercedes']
+  var audiModelArray = ["Audi_A1", "Audi_A3", "Audi_A4", "Audi_A5", "Audi_A6", "Audi_A7", "Audi_A8", "Audi_Q3", "Audi_Q5", "Audi_Q7", "Audi_R8", "Audi_RS", "Audi_TT"]
+  var bmwModelArray = ["1er-Reihe", "2er Reihe", "3er-Reihe", "4er Reihe", "5er-Reihe", "6er-Reihe", "7er-Reihe", "8er-Reihe"]
+
+  var getElement = (array: Array<String>) => {
+    const list = array.map((name) => <option>{name}</option>);
+    return list;
+  }
+
+  var getModel = () => {
+    if (activBrand == "Audi") {
+      return getElement(audiModelArray)
+    } else if (activBrand == "BMW") {
+      return getElement(bmwModelArray)
+    } else {
+      return <option>error</option>;
+    }
   }
 
   return (
@@ -14,18 +31,14 @@ const Home: NextPage = () => {
         <h1>Fahrzeugauswahl</h1>
       </div>
       <div className={styles.choosing}>
-        <select>
-          <option value='wahl'>---</option>
-          <option value='BMW'>BMW</option>
-          <option value='Audi'>Audi</option>
-          <option value='Mercedes'>Mercedes</option>
+        <select onChange={(event) => { setActivBrand(event.target.value) }}>
+          {
+            getElement(herstellerArray)
+          }
         </select>
 
-        <select onSelect={console}>
-          <option value='wahl'>---</option>
-          <option value='BMW'>BMW</option>
-          <option value='Audi'>Audi</option>
-          <option value='Mercedes'>Mercedes</option>
+        <select>
+          {getModel()}
         </select>
 
         <select>
@@ -36,6 +49,7 @@ const Home: NextPage = () => {
         </select>
       </div>
       <div className={styles.content}>
+        <img alt='Car'></img>
       </div>
     </div>
   )
